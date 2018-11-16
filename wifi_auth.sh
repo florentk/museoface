3cf246e6433a4e253d76867096b3532014fbad2c
Florent Kaisser <florent@kaisser.name>
2018-11-16 15:17:23 +0100

    add curl timeout

--- wifi_auth.sh
+++ wifi_auth.sh
@@ -1,3 +1,3 @@
-curl -v -L  http://172.20.24.20/upload/custom/cp_guest_musee/auth/index.html
+curl -m 10 -v -L  http://172.20.24.20/upload/custom/cp_guest_musee/auth/index.html
 sleep 1
-curl -v -F cmd=authenticate -F email=contact@museomix.fr  -L  http://172.20.24.20/auth/index.html/u
+curl -m 10 -v -F cmd=authenticate -F email=contact@museomix.fr  -L  http://172.20.24.20/auth/index.html/u
